ccee42f24e4fb471d390375f0106ed872347ebb0
david <davidsk2004@gmail.com>
2026-01-11 13:24:55 +0100

    fixed some things

--- src/main/java/com/skupina1/location/userRepo/EntityManagerProducer.java
+++ src/main/java/com/skupina1/location/userRepo/EntityManagerProducer.java
@@ -1,29 +1,24 @@
 package com.skupina1.location.userRepo;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.enterprise.inject.Produces;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 @RequestScoped
 public class EntityManagerProducer {
 
-    private static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("postgisPU");
+    // private static final EntityManagerFactory emf =
+    //         Persistence.createEntityManagerFactory("postgisPU");
 
-    @Produces
-    @RequestScoped
-    @MyEM
-    public EntityManager createEntityManager() {
-        return emf.createEntityManager();
-    }
+    // @Produces
+    // @RequestScoped
+    // @MyEM
+    // public EntityManager createEntityManager() {
+    //     return emf.createEntityManager();
+    // }
 
-    // Add cleanup method if needed
-    public void close() {
-        if (emf != null && emf.isOpen()) {
-            emf.close();
-        }
-    }
+    // // Add cleanup method if needed
+    // public void close() {
+    //     if (emf != null && emf.isOpen()) {
+    //         emf.close();
+    //     }
+    // }
 }
--- src/main/java/com/skupina1/location/userRepo/MyEM.java
+++ src/main/java/com/skupina1/location/userRepo/MyEM.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Qualifier             // Marks this annotation as a CDI qualifier
-@Retention(RUNTIME)    // Available at runtime
-@Target({FIELD, TYPE, METHOD, PARAMETER}) // Can be used on fields, classes, methods, parameters
-public @interface MyEM {}  // Name of the qualifier
+// @Qualifier             // Marks this annotation as a CDI qualifier
+// @Retention(RUNTIME)    // Available at runtime
+// @Target({FIELD, TYPE, METHOD, PARAMETER}) // Can be used on fields, classes, methods, parameters
+// public @interface MyEM {}  // Name of the qualifier
